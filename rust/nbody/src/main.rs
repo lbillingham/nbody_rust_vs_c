c555a76088f9d6561035f1836d362e5eff49b34e
Laurence Billingham <laurence@travelnest.com>
2019-12-29 17:53:14 +0000

    :sparkles: add (unused) energy conserving function

--- rust/nbody/src/main.rs
+++ rust/nbody/src/main.rs
@@ -88,9 +88,42 @@ unsafe fn offset_Momentum(bodies: *mut body) {
     for i in 0..BODIES_COUNT {
         for m in 0..3 {
             (*bodies.add(0)).velocity[m] -=
-                (*bodies.add(i)).velocity[m] * ((*bodies.add(i)).mass) / SOLAR_MASS;
+                (*bodies.add(i)).velocity[m] * (*bodies.add(i)).mass / SOLAR_MASS;
         }
     }
+}
+
+unsafe fn output_Energy(bodies: *mut body) {
+    let mut energy: f64 = 0.;
+    for i in 0..BODIES_COUNT {
+        energy += (*bodies.add(i)).mass
+            * ((*bodies.add(i)).velocity[0] * (*bodies.add(i)).velocity[0]
+                + (*bodies.add(i)).velocity[1] * (*bodies.add(i)).velocity[1]
+                + (*bodies.add(i)).velocity[2] * (*bodies.add(i)).velocity[2]);
+
+        for j in i + 1..BODIES_COUNT {
+            let mut position_Delta = [mem::MaybeUninit::<f64>::uninit(); 3];
+
+            for m in 0..3 {
+                position_Delta[m]
+                    .as_mut_ptr()
+                    .write((*bodies.add(i)).position[m] - (*bodies.add(j)).position[m])
+            }
+            // Unchchecked conversion between _any_ two types
+            //      as long as they're the same size
+            let position_Delta: [f64; 3] = mem::transmute(position_Delta);
+
+            energy -= (*bodies.add(i)).mass * (*bodies.add(j)).mass
+                / f64::sqrt(
+                    position_Delta[0] * position_Delta[0]
+                        + position_Delta[1] * position_Delta[1]
+                        + position_Delta[2] * position_Delta[2],
+                )
+        }
+    }
+
+    // Output the total energy of the system.
+    println!("{:.9}", energy);
 }
 
 fn main() {
